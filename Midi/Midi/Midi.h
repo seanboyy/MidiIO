b24058c257ed55a1cd91247c6e7782ff39180f65
Seanboyy <sbamford12@gmail.com>
2017-10-19 13:05:19 -0400

    Fixed problem with unresolved dependencies

--- Midi/Midi/Midi.h
+++ Midi/Midi/Midi.h
@@ -147,4 +147,21 @@ namespace mid {
 	private:
 		std::vector<Chunk> chunks;
 	};
+
+	template<typename T>
+	void StreamingHelper::streamValueToBitString(uint& cursor, char*& bitString, T value) {
+		for (uint i = 0; i < sizeof(value); i++) {
+			bitString[cursor] = (value << (8 * i)) >> (8 * (sizeof(value) - 1));
+			cursor++;
+		}
+	}
+
+	template<typename T>
+	void StreamingHelper::streamObjectToBitString(uint& cursor, char*& bitString, T object, uint length) {
+		char* objStr = object.toBitString();
+		for (uint i = 0; i < length; i++) {
+			bitString[cursor] = objStr[i];
+			cursor++;
+		}
+	}
 }
